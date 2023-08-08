38fa05425283a06efd38aa6eb1e183e20c1ae37a
Anastasia D <anagemixia@gmail.com>
2023-08-08 14:38:53 +0300

    add format option to gendiff

--- gendiff.js
+++ gendiff.js
@@ -5,5 +5,7 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('')
+    .arguments('<filepath1>, <filepath2>')
+    .option('-f, --format <type>', 'output format')
 
 program.parse();
