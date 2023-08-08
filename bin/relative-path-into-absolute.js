a4c913b5ebc26f9fb5ab781dc8055e3dfe016066
Anastasia D <anagemixia@gmail.com>
2023-08-08 16:36:53 +0300

    add function to program

---
+++ bin/relative-path-into-absolute.js
@@ -0,0 +1,10 @@
+import path from 'node:path';
+import process from 'process';
+
+export default (givenPath) => {
+    if (givenPath.startsWith('/')) {
+        return givenPath;
+    }
+    const currentDirectory = process.cwd();
+    return path.resolve(currentDirectory, givenPath);
+}
--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,9 @@
-const { Command } = require('commander');
+#!/usr/bin/env node
+import _ from 'lodash';
+import * as fs from 'node:fs';
+import { Command } from 'commander';
+import parseJSON from './bin/parse-json.js'
+import makePath from './bin/relative-path-into-absolute.js'
 const program = new Command();
 
 program
@@ -9,3 +14,42 @@ program
     .option('-f, --format <type>', 'output format')
 
 program.parse();
+
+const options = program.opts();
+const outputFormat = options.format;
+const filepath1 = makePath(program.args[0]);
+const filepath2 = makePath(program.args[1]);
+const file1RawData = fs.readFileSync(filepath1);
+const file2RawData = fs.readFileSync(filepath2);
+const file1Parsed = parseJSON(file1RawData);
+const file2Parsed = parseJSON(file2RawData);
+const file1Keys = Object.keys(file1Parsed);
+const file2Keys = Object.keys(file2Parsed);
+const file1KeysSorted = _.sortBy(file1Keys);
+const file2KeysSorted = _.sortBy(file2Keys);
+const firstSort = file1KeysSorted.reduce((acc, key) => {
+    const value1 = file1Parsed[key];
+    const value2 = file2Parsed[key];
+    if (!file2KeysSorted.includes(key)) {
+        acc.push(`- ${key}: ${value1}`);
+        return acc;
+    }
+    if (value1 === value2) {
+        acc.push(`  ${key}: ${value1}`);
+        return acc;
+    }
+    acc.push(`- ${key}: ${value1}`);
+    acc.push(`+ ${key}: ${value2}`);
+    return acc;
+}, []);
+const secondSort = file2KeysSorted.reduce((acc, key) => {
+    const value2 = file2Parsed[key];
+    if (!file1KeysSorted.includes(key)) {
+        acc.push(`+ ${key}: ${value2}`);
+        return acc;
+    }
+    return acc;
+}, firstSort);
+
+const resultString = `{\n${secondSort.join('\n')}\n}`
+console.log(resultString);
