9da10dbfdf1544e3c6082a2b7dbf4cc824cc17c5
Anastasia D <anagemixia@gmail.com>
2023-08-10 01:22:33 +0300

    stylish spacing fix

--- bin/stylish.js
+++ bin/stylish.js
@@ -14,15 +14,14 @@ const style = (data, spacing) => {
                     prefix = spacer.repeat(2);
                     break;
             }
-            const preValueString = `${spacer.repeat(spacing-2)}${prefix}${key}:`;
+            const preValueString = `${spacer.repeat(spacing)}${prefix}${key}:`;
             if (Array.isArray(value)) {
                 return `${acc}\n${preValueString} ${style(value, spacing + 4)}`;
             } else {
                 return `${acc}\n${preValueString} ${value}`;
             }
     }, '');
-    const endSpace = (spacing - 4) < 0? (spacing - 2) : (spacing - 4);
-    return `{${preResult}\n${spacer.repeat(endSpace)}}`;
+    return `{${preResult}\n${spacer.repeat((spacing - 2))}}`;
 };
 
 export default (array) => style(array, 2);
