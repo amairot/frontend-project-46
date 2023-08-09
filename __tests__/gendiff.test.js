1ef3f381a03d2ff914e0801e87cecd6eeef7c2c1
Anastasia D <anagemixia@gmail.com>
2023-08-09 19:27:25 +0300

    update test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'url';
+import path from 'node:path';
 import gendiff from '../bin/gendiff.js';
 
 const idealResult = `{
@@ -9,8 +11,12 @@ const idealResult = `{
   + verbose: true
 }`;
 
-const fullFilepath1 = '/mnt/f/prj/frontend-project-46/src/file1.json';
-const fullFilepath2 = '/mnt/f/prj/frontend-project-46/src/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const fullFilepath1 = getFixturePath('file1.json');
+const fullFilepath2 = getFixturePath('file2.json');
 
 test('check result', () => {
   expect(gendiff(fullFilepath1, fullFilepath2)).toBe(idealResult);
