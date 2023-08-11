95d466609acf58a19df08eaa43846e84b1dddd6d
Anastasia D <anagemixia@gmail.com>
2023-08-11 20:17:40 +0300

    add tests for yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,14 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'node:path';
 import gendiff from '../src/gendiff.js';
 
-const idealResultYaml = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const idealResultJson = `[{"key":"common","children":[{"key":"follow","value":false,"status":"added"},{"key":"setting1","value":"Value 1","status":"unchanged"},{"key":"setting2","value":200,"status":"removed"},{"key":"setting3","value":null,"status":"updated","previousValue":true},{"key":"setting4","value":"blah blah","status":"added"},{"key":"setting5","value":[{"key":"key5","value":"value5","status":"unchanged"}],"status":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","value":"so much","status":"updated","previousValue":""}],"status":"unchanged"},{"key":"key","value":"value","status":"unchanged"},{"key":"ops","value":"vops","status":"added"}],"status":"unchanged"}],"status":"unchanged"},{"key":"group1","children":[{"key":"baz","value":"bars","status":"updated","previousValue":"bas"},{"key":"foo","value":"bar","status":"unchanged"},{"key":"nest","value":"str","status":"updated","previousValue":[{"key":"key","value":"value","status":"unchanged"}]}],"status":"unchanged"},{"key":"group2","value":[{"key":"abc","value":12345,"status":"unchanged"},{"key":"deep","children":[{"key":"id","value":45,"status":"unchanged"}],"status":"unchanged"}],"status":"removed"},{"key":"group3","value":[{"key":"deep","children":[{"key":"id","children":[{"key":"number","value":45,"status":"unchanged"}],"status":"unchanged"}],"status":"unchanged"},{"key":"fee","value":100500,"status":"unchanged"}],"status":"added"}]`;
 
 const idealResultPlain = `Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
@@ -73,8 +66,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const fileJson1 = getFixturePath('file3.json');
-const fileJson2 = getFixturePath('file4.json');
+const fileJson1 = getFixturePath('file1.json');
+const fileJson2 = getFixturePath('file2.json');
 const fileYaml1 = getFixturePath('file1.yaml');
 const fileYaml2 = getFixturePath('file2.yaml');
 
@@ -86,7 +79,19 @@ test('check json to plain', () => {
   expect(gendiff(fileJson1, fileJson2, 'plain')).toBe(idealResultPlain);
 });
 
+test('check json to json', () => {
+  expect(gendiff(fileJson1, fileJson2, 'json')).toBe(idealResultJson);
+});
+
+
+test('check yaml to stylish', () => {
+  expect(gendiff(fileYaml1, fileYaml2, 'stylish')).toBe(idealResultStylish);
+});
+
+test('check yaml to plain', () => {
+  expect(gendiff(fileYaml1, fileYaml2, 'plain')).toBe(idealResultPlain);
+});
 
 test('check yaml to json', () => {
-  expect(gendiff(fileYaml1, fileYaml2, 'stylish')).toBe(idealResultYaml);
+  expect(gendiff(fileYaml1, fileYaml2, 'json')).toBe(idealResultJson);
 });
